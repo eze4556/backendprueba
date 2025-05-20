ee56a19f81c59526ad5b50ee7fb827491f46add3
willd3r <wsosa4124@gmail.com>
2025-05-19 23:40:45 -0300

    .

--- src/environments/environments.ts
+++ src/environments/environments.ts
@@ -4,8 +4,7 @@ const env = load({
   ENVIRONMENT: String,
   MONGO_DB_LOCAL_URI: String,
   MONGO_DB_PRODUCTION_URI: String,
-  AWS_BUCKET_DEV: String,
-  AWS_BUCKET_PROD: String,
+  BUCKET_NAME: String,
   AWS_ID: String,
   AWS_SECRET_KEY: String,
   AWS_URL: String,
@@ -15,14 +14,14 @@ const environment = () => {
   return env.ENVIRONMENT === 'development'
     ? {
         MONGO_DB_URI: env.MONGO_DB_LOCAL_URI,
-        AWS_BUCKET: env.AWS_BUCKET_DEV,
+        AWS_BUCKET: env.BUCKET_NAME,
         AWS_SECRET_KEY: env.AWS_SECRET_KEY,
         AWS_ID: env.AWS_ID,
         AWS_URL: env.AWS_URL,
       }
     : {
         MONGO_DB_URI: env.MONGO_DB_PRODUCTION_URI,
-        AWS_BUCKET: env.AWS_BUCKET_PROD,
+        AWS_BUCKET: env.BUCKET_NAME,
         AWS_SECRET_KEY: env.AWS_SECRET_KEY,
         AWS_ID: env.AWS_ID,
         AWS_URL: env.AWS_URL,
